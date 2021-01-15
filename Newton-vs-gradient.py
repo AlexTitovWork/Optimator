0f5e07c2b1aa05aaa628a5db2596e29f7251a2a0
alext <alexeytitovwork@gmail.com>
2021-01-15 22:16:51 +0300

    - good plot

--- Newton-vs-gradient.py
+++ Newton-vs-gradient.py
@@ -17,50 +17,61 @@ from matplotlib.pyplot import plot, ion, show
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
-mu, sigma = 0, 2  # mean and standard deviation
+mu, sigma = 0, 0.5  # mean and standard deviation
 
 # ################################################################################
-# Gradient or Cauchy method
+# Gradient descent or Cauchy method
 # First order method
-def gradient_descent(max_iterations, threshold, XY_init,
-                     obj_func, grad_func, extra_param=[],
-                     learning_rate=0.05, momentum=0.8):
+def gradient_descent(max_iters, threshold, XY_init,
+                     func, grad_func,
+                     learning_rate=0.05, extra_param=[]):
     X, Y = XY_init
     w = np.array([X, Y])
     w_history = X, Y
-    f_history = obj_func(X, Y, extra_param)
+    f_history = func(X, Y, extra_param)
     delta_w = np.zeros(XY_init.shape)
     i = 0
-    diff = 1.0e9
+    # start diff |f2 - f1| for stop criteria
+    diff_f = 1.0e10
+    eps_history_f = np.array([0.0])
+    eps_history_xy = np.array([0.0 , 0.0])
 
-    while i < max_iterations and diff > threshold:
+    while i < max_iters and diff_f >= threshold:
         delta_w = -learning_rate * grad_func(w[0], w[1], extra_param)
         w = w + delta_w
         # store the history of w and f
         w_history = np.vstack((w_history, w))
-        f_history = np.vstack((f_history, obj_func(w[0], w[1], extra_param)))
+        f_history = np.vstack((f_history, func(w[0], w[1], extra_param)))
         i += 1
-        diff = np.absolute(f_history[-1] - f_history[-2])
+        diff_f = np.absolute(func(3.0, 2.0,extra_param) - f_history[-1])
+        # diff_f = np.absolute(f_history[-1] - f_history[-2])
+        diff_xy = np.absolute(w_history[-1] - w_history[-2])
 
-    return w_history, f_history
+        eps_history_f = np.vstack((eps_history_f, diff_f))
+        eps_history_xy = np.vstack((eps_history_xy, diff_xy))
+
+    return w_history, f_history, eps_history_f, eps_history_xy
 
 # ################################################################################
 # Newton's method
 # Second order method
-def Newtons_descent(max_iterations, threshold, XY_init,
-                     obj_func, grad_func, second_grad, extra_param=[],
-                     learning_rate=0.05, momentum=0.8):
+def Newtons_method(max_iters, threshold, XY_init,
+                     func, grad_func, second_grad,
+                     learning_rate=0.05, extra_param=[]):
     X, Y = XY_init
     w = np.array([X, Y])
     w_history_N = X, Y
-    f_history_N = obj_func(X, Y, extra_param)
+    f_history_N = func(X, Y, extra_param)
     delta_w = np.zeros(XY_init.shape)
     i = 0
-    diff = 1.0e9
+    # start diff |f2 - f1| for stop criteria
+    diff_f = 1.0e10
+    eps_history_N = np.array([0.0])
+    eps_history_xy_N = np.array([0.0 , 0.0])
 
-    while i < max_iterations and diff > threshold:
-        print(grad_func(w[0], w[1], extra_param))
-        print(np.linalg.inv(second_grad(w[0], w[1], extra_param)))
+    while i < max_iters and diff_f >= threshold:
+        # print(grad_func(w[0], w[1], extra_param))
+        # print(np.linalg.inv(second_grad(w[0], w[1], extra_param)))
         # Inverse Hessian matrix of second derivatives.
         inverseHessian = np.linalg.inv(second_grad(w[0], w[1], extra_param))
         # Hessian step calculation, it is not define descent direction.
@@ -73,12 +84,17 @@ def Newtons_descent(max_iterations, threshold, XY_init,
         w = w + delta_w
         # store the history of w and f
         w_history_N = np.vstack((w_history_N, w))
-        f_history_N = np.vstack((f_history_N, obj_func(w[0], w[1], extra_param)))
+        f_history_N = np.vstack((f_history_N, func(w[0], w[1], extra_param)))
+
         i += 1
-        diff = np.absolute(f_history_N[-1] - f_history_N[-2])
+        diff_f = np.absolute(func(3.0, 2.0,extra_param) - f_history_N[-1])
+        # diff_f = np.absolute(f_history_N[-1] - f_history_N[-2])
 
-    return w_history_N, f_history_N
+        diff_xy = np.absolute(w_history_N[-1] - w_history_N[-2])
 
+        eps_history_N = np.vstack((eps_history_N, diff_f))
+        eps_history_xy_N= np.vstack((eps_history_xy_N, diff_xy))
+    return w_history_N, f_history_N, eps_history_N, eps_history_xy_N
 
 # ################################################################################
 def plot_function(noised=False):
@@ -100,9 +116,9 @@ def plot_function(noised=False):
     ax = fig.gca(projection='3d')
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                            linewidth=0, antialiased=False)
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
+    ax.set_xlabel('X ')
+    ax.set_ylabel('Y ')
+    ax.set_zlabel('Z ')
     return X, Y, Z
 
 # ################################################################################
@@ -142,40 +158,49 @@ def second_grad(X, Y, extra=[]):
     return np.array([[dx2, dxy], [dyx, dy2]])
 
 # ################################################################################
-# gradient descent
+# Start main code
+# ################################################################################
 X, Y, Z = plot_function(noised=True)
 rand = np.random.RandomState(23)
-XY_init = rand.uniform(-0.5, 0.5, 2)
+# XY_init = rand.uniform(-0.5, 0.5, 2)
 # XY_init = np.array([3.5, -2])
 # XY_init = np.array([-2, -1.9])
+XY_init = np.array([0.03, 1.9])
 
 learning_rates = [0.05, 0.2, 0.5, 0.8]
 max_iter = 500
-threshold = 0.1
-w_history, f_history = gradient_descent(max_iter, threshold, XY_init, f, grad, [], learning_rate=0.001, momentum=0.8)
+threshold = 0.0001
+# Gradient descent
+w_history, f_history, eps_history, eps_history_xy_G = gradient_descent(max_iter, threshold, XY_init, f, grad,  learning_rate=0.001)
 print("Grad")
 print(w_history)
 print(f_history)
-w_history_N, f_history_N = Newtons_descent(max_iter, threshold, XY_init, f, grad,second_grad, [], learning_rate=0.5, momentum=0.8)
+# Newton descent
+w_history_N, f_history_N, eps_history_N, eps_history_xy_N = Newtons_method(max_iter, threshold, XY_init, f, grad, second_grad,  learning_rate=0.5)
 print("Newton")
 print(w_history_N)
 print(f_history_N)
 # ################################################################################
-# Plotting
+# end main code
+# ################################################################################
+# Plotting surface and trace of optimal point search
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(X, Y, Z, c='r')
-ax.scatter(w_history[:, 0], w_history[:, 1], f_history[:, 0], 'xb-')
-ax.scatter(w_history_N[:, 0], w_history_N[:, 1], f_history_N[:, 0], 'xg-')
+# mesh3d = ax.scatter(X, Y, Z, cm='b')
+mesh3d = ax.plot_wireframe(X, Y, Z)
+plt.setp(mesh3d, 'color', 'b', 'linewidth', 0.2)
+ax.scatter(w_history[:, 0], w_history[:, 1], f_history[:, 0],  marker='+', alpha=0.5, c='r')
+ax.scatter(w_history_N[:, 0], w_history_N[:, 1], f_history_N[:, 0], marker='+', alpha=0.5, c='g' )
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.set_xlabel('X ')
+ax.set_ylabel('Y ')
+ax.set_zlabel('Z ')
 # plt.plot(w_history[:, 0], w_history[:, 1],f_history, 'ro')
+
 ## coloring couttour line
 fig.show()
-
+names = ['Gradient', 'Newton']
 fig, ax = plt.subplots()
 # CS = ax.contour(X, Y, Z)
 # lev = [1, 2, 3, 4, 6, 10, 20, 40, 100, 900]
@@ -185,13 +210,75 @@ CS = plt.contour(X, Y, Z, levels=lev)
 # ax.clabel(CS, inline=1, fontsize=10, manual=manual_locations)
 ax.clabel(CS, inline=1, fontsize=8)
 ax.set_title('Himmelblau\'s function')
-ax.plot(w_history[:, 0], w_history[:, 1],  'b*-')
-ax.plot(w_history_N[:, 0], w_history_N[:, 1],  'g+-')
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
+ax.plot(w_history[:, 0], w_history[:, 1],  'r*-', label=names[0])
+ax.plot(w_history_N[:, 0], w_history_N[:, 1],  'g+-', label=names[1])
+plt.legend()
+ax.set_xlabel('X ')
+ax.set_ylabel('Y ')
 fig.set_figwidth(12)    #  ширина и
 fig.set_figheight(6)    #  высота "Figure"
-show()
 fig.savefig('images//Figure_4.png')
+print("Plotting contour complete")
 
-print("Plotting complete")
+# ################################################################################
+# Convergence of function f(x,y) plotting: Newton vs Gradient
+plt.figure(figsize=(4, 2))
+plt.subplot(411)
+e_Gradent_line = plt.plot(eps_history, label = 'Gradient')
+plt.setp(e_Gradent_line, 'color', 'r', 'linewidth', 1.1)
+# plt.xlabel('n, iterations')
+plt.ylabel('$\epsilon$ ')
+# plt.title('$\epsilon$,  Gradient')
+plt.grid(True)
+plt.axis([0, 300, 0, 100])
+plt.legend()
+
+
+plt.subplot(412)
+e_Newton_line = plt.plot(eps_history_N, label = 'Newton')
+plt.setp(e_Newton_line, 'color', 'g', 'linewidth', 1.1)
+# plt.xlabel('n, iterations')
+plt.ylabel('$\epsilon$')
+# plt.title('$\epsilon$, Newton')
+plt.grid(True)
+plt.axis([0, 300, 0, 100])
+plt.legend()
+
+# plt.set_xlabel('X Label')
+# plt.set_ylabel('Y Label')
+plt.suptitle('Functions and arguments convergence ')
+
+# Convergence of argument [x,y] plotting: Newton vs Gradient
+
+# names = ['eps Gradient', 'eps Newton']
+plt.subplot(413)
+# e_Gradent_line = plt.plot(eps_history_xy_G)
+e_Gradent_line = plt.plot(w_history[:,0], label = 'x')
+plt.setp(e_Gradent_line, 'color', 'r', 'linewidth', 1.2)
+e_Gradent_line = plt.plot(w_history[:,1], label = 'y')
+plt.setp(e_Gradent_line, 'color', 'r', 'linewidth', 1.2)
+# plt.xlabel('n, iterations')
+plt.ylabel('Gradient desc. args, $[x,y]$')
+# plt.title('Conv. to optimal argument,  Gradient')
+plt.grid(True)
+plt.axis([0, 300, 0, 4])
+plt.legend()
+
+
+plt.subplot(414)
+# e_Newton_line = plt.plot(eps_history_xy_N)
+e_Newton_line = plt.plot(w_history_N[:,0],  label = 'x')
+plt.setp(e_Newton_line, 'color', 'lime', 'linewidth', 1.2)
+e_Newton_line = plt.plot(w_history_N[:,1],  label = 'y')
+plt.setp(e_Newton_line, 'color', 'g', 'linewidth', 1.2)
+plt.xlabel('n, iterations')
+plt.ylabel('Newton args, $[x,y]$')
+# plt.title('Conv. to optimal argument,  Newton')
+plt.grid(True)
+plt.axis([0, 300, 0, 4])
+plt.legend()
+
+show()
+print("Plotting convergence complete")
+
+
